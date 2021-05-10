71a6009b3b9d0e941573c77da93820703a9d7b48
Juani Diaz Bengolea <juanidb.95@gmail.com>
2021-05-10 13:56:02 -0300

    now I import the functions to create the elements, html file is almost empty

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log('simple statement')
+// importing the functions
+
+import {newTitle, newParag, newImg} from './page-load.js';
+
+const content = document.getElementById('content');
+
+content.appendChild(newTitle('myRestaurant Page'));
+
+content.appendChild(newImg('../src/images/myRestaurant.jpeg', 'restaurant image'));
+
+content.appendChild(newParag('Dining out presents a great opportunity to unwind, relax and enjoy a delicious meal in a great atmosphere. While this is what most people are looking for when they decide to dine out, it is not always what they get. There are some very good restaurants, but unfortunately, they are few and far between. People will always make a return visit when they enjoy the dining experience and it is to everyone’s advantage for the restaurant to up its game! Following are some of the qualities or characteristics that distinguish between a great restaurant and other restaurants.'));
---
+++ src/page-load.js
@@ -0,0 +1,30 @@
+// function to create a new title
+
+const newTitle = content => {
+    const title = document.createElement('h1');
+    title.innerHTML = content;
+    title.setAttribute('class', 'title');
+    return title;
+}
+
+// function to add a paragraph
+
+const newParag = content => {
+    const parag = document.createElement('p');
+    parag.innerHTML = content;
+    parag.setAttribute('class', 'paragraph');
+    return parag
+}
+
+// function to add an image
+
+const newImg = (imgSrc, alt) => {
+    const img = document.createElement('img');
+    img.setAttribute('src', imgSrc);
+    if (alt) img.setAttribute('alt', alt);
+    return img
+}
+
+// exporting the functions
+
+export {newTitle, newParag, newImg}
